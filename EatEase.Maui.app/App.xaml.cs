97890284f8a7034959e4d84421575a9b96590b50
bzorec <blaz.zorec2002@gmail.com>
2023-11-15 09:43:02 +0100

    initial commit

---
+++ EatEase.Maui.app/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EatEase.Maui.app;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ EatEase.Maui.app/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EatEase.Maui.app;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
