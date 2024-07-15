dd0b92902d2dd8021327970804536823fe4bc37b
bzorec1 <40613311+bzorec1@users.noreply.github.com>
2024-07-15 21:53:35 +0200

    added navigation and footer html and css

--- Group.md
+++ Group.md
@@ -1,3 +1,4 @@
+
 # DOMAIN MODEL
 
 ```json
--- src/EatEase.Blazor.Web/Components/Navigation.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace EatEase.Blazor.Web.Components;
-
-public partial class Navigation : ComponentBase
-{
-}
